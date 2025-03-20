75bd40ffda499bbd2f0355e7bb369a617c746962
jubchaves <juliachaves@usp.br>
2025-03-20 09:28:29 -0300

    Download Ktor Framework
    Signed-off-by: jubchaves <juliachaves@usp.br>

---
+++ src/main/kotlin/Serialization.kt
@@ -0,0 +1,23 @@
+package com.example
+
+import com.codahale.metrics.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.concurrent.TimeUnit
+
+fun Application.configureSerialization() {
+    routing {
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
